fbe860da1adbb514e876003819b7714456f17003
sksinthu <sksinthu15@gmail.com>
2026-08-19 13:36:52 +0530

    fix: update next.config.js and add vercel.json framework preset

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  generateBuildId: async () => {
-    return `build-${Date.now()}`;
-  },
   images: {
     remotePatterns: [
       {
